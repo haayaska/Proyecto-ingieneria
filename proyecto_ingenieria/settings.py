0c769fdba3d65cd05f5380ef13649e9eec2d6d9d
haayaska <127577576+haayaska@users.noreply.github.com>
2023-09-24 17:41:17 -0300

    A

--- proyecto_ingenieria/settings.py
+++ proyecto_ingenieria/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c=gi8g-i9qf!)*w@5^$wv(shv_s_4#dr4bpgxy7(h7q7d68n*x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
