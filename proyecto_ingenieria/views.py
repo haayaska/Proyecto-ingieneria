59c4dff074e205127f42d0e6b5b3de2657b29f6a
JoxyM0xy <johnalvaradotoro@hotmail.com>
2023-11-08 02:13:08 -0300

    Cambie casi toda la estructura de los css, asi que preguntenme

--- proyecto_ingenieria/views.py
+++ proyecto_ingenieria/views.py
@@ -5,36 +5,15 @@ import requests
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
+import time
 
-
-def home(request): #Primera vista
-    1
-    tiempo= "<h1>{0}</h1>".format(datetime.datetime.now().strftime("%A: %d/%m/%Y "))
-    inicio = """ 
-    <html>
-    <body>
-        <h1>The LECT Team is working on this web<h1>
-        <h2> Cool things are comming soon...<h2>
-    <body>
-    <html>
-    """
-    texto = inicio + '\n' + tiempo
-    return HttpResponse(texto)
-
-'''def Presentacion(request):
-    presentacionex = open('C:/Proyecto-ingieneria/plantillas/Presentacion.html') #el path no se detecta, podrian probar si funciona y avisar
-    template= Template(presentacionex.read()) #cuando lo vayan a probar cambien al path en donde se encuentra el archivo presentacion.html
-    presentacionex.close()
-    contexto = Context()
-    docfinal = template.render(contexto)
-    return HttpResponse(docfinal) #Habia un error, no se generaba una httpresponse '''
 
 def Presentacion(request):
     return render(request, 'main/Presentacion.html')
 
 def clima(request):
-    lat = '-33.0658'
-    lon = '-71.3289' #cordenadas de villa alemana como prueba
+    lat = '-33.0475'
+    lon = '-71.4422' 
     api_key = str(settings.OPENWEATHERKEY)
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
     response = requests.get(url)
@@ -57,11 +36,12 @@ def clima(request):
                       }
     for llave in tipos_climas:
         if llave==descripcion:
-            descripcion= tipos_climas[llave]
+            descripcion= (tipos_climas[llave])[0]
     ciudad = data.get('name')
     context = {
     'temperatura': temperatura_celcius,
     'descripcion': descripcion,
     'ciudad': ciudad
     }
-    return render(request, 'clima.html', context)
+    return render(request, 'main/clima.html', context)
+
