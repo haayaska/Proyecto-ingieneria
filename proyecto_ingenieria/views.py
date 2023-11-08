444463c84821564da38e87f575334829b25fd77e
JoxyM0xy <johnalvaradotoro@hotmail.com>
2023-11-08 20:10:30 -0300

    cambios menores

--- proyecto_ingenieria/views.py
+++ proyecto_ingenieria/views.py
@@ -1,17 +1,18 @@
 from django.http import HttpResponse
-import datetime
 from django.template import Template, Context
-import requests
 from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render
 import time
+import datetime
+import requests
+
 
 def Presentacion(request):
     return render(request, 'main/Presentacion.html')
 
 def consumo(request):
-    #aqui esta la api de openweather
+    #aqui esta la api de openweather:
     lat = '-33.0333'
     lon = '-71.6667' 
     api_key = str(settings.OPENWEATHERKEY)
@@ -20,18 +21,18 @@ def consumo(request):
     datas = response.json()
     temperatura_kelvin = datas.get('main', {}).get('temp') #esto sacara del json el valor de la temperatura en kelvin
     temperatura_celcius = round(temperatura_kelvin - 273.15) #transformara la temperatura de kelvin a celcius
-    descripcion = datas.get('weather', [])[0].get('description')
-    tipos_climas = {"clear sky": ('Despejado', 0), # 0 significa "Apaga las luces"
+    descripcion = datas.get('weather', [])[0].get('description') #sacara del json la descripcion del clima
+    tipos_climas = {"clear sky": ('Despejado', 0), 
                     "partly cloudy": ('Parcialmente Nublado', 1 ),
                     'overcast clouds': ('Nublado'),
                     "clouds": ('Nublado', 1 ),
                     "fog": ('Niebla', 1),
                     "mist": ('Neblina', 1 ),
-                    "drizzle": ('Llovizna', 2), # 2 significa "Intenta apagar otros dispositivos para ahorrar energia"
+                    "drizzle": ('Llovizna', 2), 
                     "rain": ('Lluvioso', 2),
                     'moderate rain': ('Lluvia Moderada', 2),
                     "showers": ('Chubascos', 2),
-                    "snow": ('Nieve', 3 ), # 3 Si utilizas dispositivos de calefaccion apaga las luces cuando sea necesario
+                    "snow": ('Nieve', 3 ), 
                     "thunderstorm": ('Tormenta Electrica', 4),
                       }
     for llave in tipos_climas:
@@ -39,15 +40,17 @@ def consumo(request):
             descripcion= (tipos_climas[llave])[0]
             numero= (tipos_climas[llave])[1]
     ciudad = datas.get('name')
-    #aqui esta la api de SMARTTHINGS
+    #aqui esta la api de SMARTTHINGS:
     deviceId = str(settings.DEVICE_ID)
     tk= 'Bearer ' + str(settings.SMART_THINGSTK)
     url= f'https://api.smartthings.com/v1/devices/{deviceId}/components/main/capabilities/switch/status'
     response = requests.get(url, headers={"Authorization": tk})
     datos = response.json()
     estado= datos.get('switch', {}).get('value')
-    if (numero== 0) and estado == "on":
+    hora_actual = 8
+    if (numero== 0) and (estado == "on") and (8<=hora_actual<21):
         apagadoAuto(request)
+        estado = "off"
     #diccionario context con todos los datos para la pagina de consumo
     context = {
     'temperatura': temperatura_celcius,
@@ -90,7 +93,6 @@ def clima(request):
     'descripcion': descripcion,
     'ciudad': ciudad
     }
-    print(contextClima)
     return render(request, 'main/clima.html', contextClima)
 
 def estadoLuz(request):
@@ -100,7 +102,6 @@ def estadoLuz(request):
     response = requests.get(url, headers={"Authorization": tk})
     datos = response.json()
     contextAmpolleta= {"estado": datos.get('switch', {}).get('value')}
-    print(contextAmpolleta)
     return render(request, 'main/estado.html', contextAmpolleta)
 
 
@@ -119,6 +120,5 @@ def apagadoAuto(request):
     }
     url= f'https://api.smartthings.com/v1/devices/{deviceId}/commands'
     response = requests.post(url,json=body, headers={"Authorization": tk})
-    print(response.json())
     texto= 'aceptado'
     return(texto)
