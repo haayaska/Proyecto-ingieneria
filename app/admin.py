714686a768efd20b101af559c8b0c48267f01abd
JoxyM0xy <johnalvaradotoro@hotmail.com>
2023-11-13 16:35:35 -0300

    lol

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display=('username', 'Smart_tkn', 'Smart_id')
+    search_fields = ('username',)
