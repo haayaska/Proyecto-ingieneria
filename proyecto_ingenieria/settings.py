882d8d0a14c3d643f8e033dde799d26d28965435
flawless0011 <franciscopino1236@gmail.com>
2023-11-24 14:01:33 -0300

    aaaaa

--- proyecto_ingenieria/settings.py
+++ proyecto_ingenieria/settings.py
@@ -134,6 +134,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'img'),
     )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
